8fceed50625f2ec56f4613945b78a9a75cd7989e
Maduki-tech <maduki.business@gmail.com>
2023-05-12 13:12:20 +0200

    added function tutorial

---
+++ Functions/functions.ts
@@ -0,0 +1,35 @@
+function createFunctionInJavaScript() {
+  // function declaration
+}
+
+const createArrowFunctionInJavaScript = () => {
+  // arrow function
+}
+
+function callFunctionInJavaScript() {
+  createFunctionInJavaScript();
+  createArrowFunctionInJavaScript();
+}
+
+function createFunctionInTypeScript(parameter1: string, parameter2: number): boolean {
+  // function declaration with explicit parameter and return types
+  return true;
+}
+
+function callFunctionInTypeScript() {
+  const result = createFunctionInTypeScript("string parameter", 1);
+  console.log(result);
+}
+
+function overloadFunctionInTypeScript(parameter: string): string;
+function overloadFunctionInTypeScript(parameter: number): number;
+function overloadFunctionInTypeScript(parameter: any): any {
+  // overloaded function declaration
+}
+
+function callOverloadedFunctionInTypeScript() {
+  overloadFunctionInTypeScript("string parameter");
+  overloadFunctionInTypeScript(1);
+  overloadFunctionInTypeScript(true);
+}
+
